48efd24011ea2bec0d286be3c181120f508daf83
Rebecca Hom <rebecca.hom001@gmail.com>
2019-06-08 18:46:20 -0700

    added basic Home page and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import './App.css';
+import Home from "./pages/Home";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "Enter a movie you want to watch."
+    }
+  };
+
+  render() {
+    return (
+      <Router>
+        <>
+          <Switch>
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </>
+      </Router>
+    );
+  }
+};
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <h1>Before You Watch</h1>
+    );
+  }
+}
+
+export default Home;
