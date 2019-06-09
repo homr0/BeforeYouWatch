4b2a031a10aa5d0e4249d58f25cccde536c43e62
Rebecca Hom <rebecca.hom001@gmail.com>
2019-06-08 19:16:28 -0700

    added Grid component

---
+++ client/src/components/Grid/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export const Container = ({children}) => {
+  return(
+    <div className="container">
+      {children}
+    </div>
+  );
+}
+
+export const Row = ({id, children}) => {
+  return(
+    <div className="row" id={id}>
+      {children}
+    </div>
+  );
+}
+
+export const Col =({ id, size, children })=> {
+  return (
+    <div className={"col " + ((size) ? size : "s12")} id={id}>
+      {children}
+    </div>
+  );
+}
