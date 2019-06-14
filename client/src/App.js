e4cb0ceb109d394bac2a83fcab144224c1d16e0e
Rebecca Hom <rebecca.hom001@gmail.com>
2019-06-13 18:14:20 -0700

    updated Home page to include text input

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import './App.css';
+
+import Header from "./components/Header";
+
 import Home from "./pages/Home";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [theme, setTheme] = useState({
+    colorNav: "blue darken-1",
+    colorBackground: "white"
+  });
+  
+  return (
+    <Router>
+      <>
+        <Header colorNav={theme.colorNav} />
 
-    this.state = {
-      message: "Enter a movie you want to watch."
-    }
-  };
-
-  render() {
-    return (
-      <Router>
-        <>
-          <Switch>
-            <Route path="/" exact component={Home} />
-          </Switch>
-        </>
-      </Router>
-    );
-  }
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "../components/Grid";
+import { Radio, Switch, Checkbox, Input, Form, Fieldset } from "../components/Form";
 
 class Home extends Component {
   render() {
@@ -9,6 +10,24 @@ class Home extends Component {
           <Col>
             <h1 className="center">Before You Watch</h1>
           </Col>
+        </Row>
+
+        <Row>
+          <Col size="m3 show-on-medium-and-up"
+            id="adspace1"
+          >
+
+          </Col>
+
+          <Col size="s12 m6">
+            <Form action="/movie/get" method="GET">
+              <Input name="movie_name" id="movie_name">Search for Movie</Input>
+            </Form>
+          </Col>
+          
+          <Col size="m3 show-on-medium-and-up"
+            id="adspace2"
+          ></Col>
         </Row>
       </Container>
     );
