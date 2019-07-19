f7cafb72d213b5e26c78d29cfcc0dbf6b7373e24
Rebecca Hom <rebecca.hom001@gmail.com>
2019-07-18 20:08:43 -0700

    added IMDb link to MovieCard

--- client/src/components/MovieCard/index.js
+++ client/src/components/MovieCard/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import {List, ListHeader, ListItem} from "../List";
 
-export const MovieListItem = ({title, year}) => <ListItem>
-  <p>{title} ({year})</p>
+export const MovieListItem = ({title, year, imdb}) => <ListItem>
+  <p><a href={"https://www.imdb.com/title/" + imdb}>{title} ({year})</a></p>
 </ListItem>
 
 export const MovieList = ({query, children}) => <List>
